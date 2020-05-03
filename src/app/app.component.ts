4091864c235929e353a487f2bcdf1b8af5349c99
podberezsky <podberezsky@my.com>
2020-05-03 19:46:17 +0300

    Первый пример стр. 54

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Plant } from './plant';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'plants';
+  plant:Plant = new Plant('тюльпан','растет на даче', 99);
 }
