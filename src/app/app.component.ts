b4d47f1b1bd5686127438e2a276665d1c163df1c
podberezsky <podberezsky@my.com>
2020-05-06 23:00:15 +0300

    исправлена ошибка в тексте стр 59

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,17 @@ import { Plant } from './plant';
 export class AppComponent {
   title = 'plants';
   plant:Plant = new Plant('тюльпан','растет на даче', 99);
+  temp:Plant = new Plant('тюльпан','растет на даче', 99);
+
+  cancel():void {
+	  this.plant.name = this.temp.name;
+	  this.plant.description = this.temp.description;
+	  this.plant.price = this.temp.price;
+  }
+
+  save():void {
+	  this.temp.name = this.plant.name;
+	  this.temp.description = this.plant.description;
+	  this.temp.price = this.plant.price;
+  }
 }
